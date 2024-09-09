1d32373d4894629258df9e253568a3d6ea158cd6
Rinaldo <junnin1998@gmail.com>
2024-09-08 22:39:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+//Lista de objetos
+const textos = [
+  {id: 1, texto: 'Teste 1'},
+  {id: 2, texto: 'Teste 2'},
+  {id: 3, texto: 'Teste 3'},
+  {id: 4, texto: 'Teste 4'},
+  {id: 5, texto: 'Teste 5'}
+];
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <ul>
+      {
+        //Renderiza a LI com a key do id e texto relacionado a cada posição da lista
+        textos.map (item => 
+          <li key={item.id}>{item.texto}</li>
+        )
+      }
+    </ul>
+   </div>
   );
+}
+
+//Tambem funcionaria dessa maneira
+/*
+<ul>
+
+  //A key precisa ser colocada na estrutura de repetição e não na função
+  textos.map (x => <Item key={x.id} item = {x}/>)
+</ul>
+
+*/
+
+function Item({item}){
+  return <li>{item.texto}</li>
 }
 
 export default App;
